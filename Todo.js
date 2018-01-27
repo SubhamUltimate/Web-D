ee3382b91dc2e30470ff068bab9702a14e360828
Subham Ray <33590388+SubhamUltimate@users.noreply.github.com>
2018-01-27 17:32:41 +0530

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,19 @@
+$("ul").on("click","li",function(){
+  $(this).toggleClass("completed");
+});
+$("ul").on("click","span",function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+$("input[type='text'").on("keypress",function(event){
+  if(event.which===13){
+    var todotext=$(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>"+todotext+"</li>");
+  };
+});
+$(".fa-plus-circle").click(function(){
+  $("input[type='text'").fadeToggle();
+});
